af191c797bfc341602532f15c13b1cfde531a6f2
Mo72med <Mo72medsalem@gmail.com>
2023-10-09 22:47:10 +0300

    add main page ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'Pages/CatalogsPages/Catalogs1Page.dart';
+
+import 'Pages/main_page/main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: CatalogPage(),
+      home: MainPage(),
     );
   }
 }
